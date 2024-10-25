c063d70963272f481f1eb09c7ad9cf93fd756e16
Jricnav1811 <jricnav1811@g.educaand.es>
2024-10-25 10:21:56 +0200

    Responsive fixed

--- frontend/turing-clothes/src/app/app.routes.ts
+++ frontend/turing-clothes/src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { RegistroComponent } from './components/registro/registro.component';
 import { IniciosesionComponent } from './components/iniciosesion/iniciosesion.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'catalog', component: CatalogoComponent},
-  {path: 'register', component: RegistroComponent},
-  {path: 'login', component: IniciosesionComponent}
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'catalog', component: CatalogoComponent },
+  { path: 'register', component: RegistroComponent },
+  { path: 'login', component: IniciosesionComponent },
 ];
