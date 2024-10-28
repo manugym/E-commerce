7e6de04f0490e6bf399ffd6d397a6650355f5006
Javier Rico Navarro <javier.riconav@gmail.com>
2024-10-28 19:41:35 +0100

    Conseguido el token en el front

--- frontend/turing-clothes/src/app/app.routes.ts
+++ frontend/turing-clothes/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { RegistroComponent } from './components/registro/registro.component';
-import { InicioSesionComponent } from './components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
@@ -10,6 +10,6 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'catalog', component: CatalogoComponent },
   { path: 'register', component: RegistroComponent },
-  { path: 'login', component: InicioSesionComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'footer', component: FooterComponent}
 ];
