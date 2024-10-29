4f8e7cff1959780d78ce52b9f567b278eb266779
Javier Rico Navarro <javier.riconav@gmail.com>
2024-10-29 18:48:35 +0100

    User funcionando sin cosas innecesarias

--- backend/TuringClothes/TuringClothes/Database/User.cs
+++ backend/TuringClothes/TuringClothes/Database/User.cs
@@ -8,21 +8,21 @@ namespace TuringClothes.Database
     [Index(nameof(Email), IsUnique = true)]
     public class User
     {
-        [Required]
+        /*[Required]
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]*/
         public long Id { get; set; }
-        [Required]
+ 
         public string Name { get; set; }
-        [Required]
+  
         public string Surname { get; set; }
-        [Required]
+
         public string Email { get; set; }
-        [Required]
+     
         public string Password { get; set; }
-        [Required]
+ 
         public string Address { get; set; }
-        [Required]
+   
 
         [DefaultValue("user")]
         public string Role { get; set; }
