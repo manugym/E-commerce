3fbebc1b8a81a714235d84e78fbc14ebf4075647
Jricnav1811 <jricnav1811@g.educaand.es>
2024-11-05 17:14:09 +0100

    Catalogo con búsqueda incorporada y filtro de nombre y precio funcionando + ascendente y descendente.

--- frontend/turing-clothes/src/app/app.routes.ts
+++ frontend/turing-clothes/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-// import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FooterComponent } from './pages/footer/footer.component';
