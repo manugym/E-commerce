4c891a4b80c63b6c466964a9d60cb27d96050d3c
albacabsan <145496884+albacabsan@users.noreply.github.com>
2024-11-06 03:06:40 +0100

    paginacion de productos en front + productos nuevos

--- frontend/turing-clothes/src/app/pages/catalogo/catalogo.component.ts
+++ frontend/turing-clothes/src/app/pages/catalogo/catalogo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { ProductDto } from '../../models/product-dto';
 import { CatalogService } from '../../services/catalog.service';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { PaginationParams } from '../../models/pagination-params';
 import { PagedResults } from '../../models/paged-results';
 import { FormsModule } from '@angular/forms';
@@ -10,7 +10,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [HeaderComponent, FormsModule],
+  imports: [HeaderComponent, FormsModule, CommonModule],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css',
 })
@@ -76,4 +76,28 @@ export class CatalogoComponent {
     this.getPagedResults();
   }
 
+  nextPage() {
+    if (this.paginationParams.pageNumber < this.pagedResults.totalNumberOfPages) {
+      this.paginationParams.pageNumber++;
+      this.getPagedResults();
+    }
+  }
+  
+  previousPage() {
+    if (this.paginationParams.pageNumber > 1) {
+      this.paginationParams.pageNumber--;
+      this.getPagedResults();
+    }
+  }
+
+  getPageNumbers(): number[] {
+    return Array.from({ length: this.pagedResults?.totalNumberOfPages }, (_, i) => i + 1);
+  }
+
+  goToPage(page: number) {
+    if (page !== this.paginationParams.pageNumber) {
+      this.paginationParams.pageNumber = page;
+      this.getPagedResults();
+    }
+  }
 }
