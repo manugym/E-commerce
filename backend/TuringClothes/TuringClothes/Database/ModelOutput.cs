902d1badd436fef0a99d0fd4833c5bb76264f715
Ancabvil <acabvil562@g.educaand.es>
2024-11-07 21:03:04 +0100

    IA completada

---
+++ backend/TuringClothes/TuringClothes/Database/ModelOutput.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.ML.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TuringClothes.Database
+{
+    public class ModelOutput
+    {
+        [ColumnName ("text")]
+        public string Text { get; set; }
+
+        [ColumnName ("label")]
+
+        public uint Label { get; set; }
+
+        [ColumnName ("PredictedLabel")]
+        public float PredictedLabel { get; set; }
+
+        [ColumnName ("Score")]
+        public float[] Score { get; set; }
+
+    }
+}
