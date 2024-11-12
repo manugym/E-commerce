316210155d5a274a87aae5f50dbbd48a7a982565
manumuno <manueljmarin@outlook.com>
2024-11-12 17:06:57 +0100

    cart entities

---
+++ backend/TuringClothes/TuringClothes/Repository/CartRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TuringClothes.Database;
+
+
+namespace TuringClothes.Repository
+{
+    public class CartRepository
+    {
+        private readonly MyDatabase myDatabase;
+        public CartRepository(MyDatabase database)
+        {
+            myDatabase = database;
+
+        }
+
+        public async Task<ActionResult> AddToCar(Product id, User identity )
+        {
+
+        }
+    }
+}
