d4e655ef23308e68bd2c20cc5c1bb00329953342
manumuno <manueljmarin@outlook.com>
2024-11-19 13:40:47 +0100

    TempOrder first steps

---
+++ backend/TuringClothes/TuringClothes/Database/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿namespace TuringClothes.Database
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public long TemporaryOrderID { get; set; }
+
+        public Product Product { get; set; }
+
+        public long ProductID { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
---
+++ backend/TuringClothes/TuringClothes/Database/TemporaryOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace TuringClothes.Database
+{
+    public class TemporaryOrder
+    {
+        public long Id { get; set; }
+
+        public long UserId { get; set; }
+
+        public ICollection<OrderDetail> Details { get; set; }
+    }
+}
