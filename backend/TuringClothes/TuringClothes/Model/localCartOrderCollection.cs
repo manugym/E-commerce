d23b54c48457fc5ab47d5f9de6cbac1f4ec62742
Jricnav1811 <jricnav1811@g.educaand.es>
2024-11-20 01:16:35 +0100

    Lógica para pasar productos del front al back de la orden temporal

---
+++ backend/TuringClothes/TuringClothes/Model/localCartOrderCollection.cs
@@ -0,0 +1,9 @@
+﻿using TuringClothes.Database;
+
+namespace TuringClothes.Model
+{
+    public class localCartOrderCollection
+    {
+        public ICollection<CartDetail> Details { get; set; }
+    }
+}
