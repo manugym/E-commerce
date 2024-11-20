fc90fda085fc47b41abc293a9636601c1a2b90a0
albacabsan <acabsan0107@g.educaand.es>
2024-11-20 17:45:43 +0100

    Revert "Lógica para pasar productos del front al back de la orden temporal"
    This reverts commit d23b54c48457fc5ab47d5f9de6cbac1f4ec62742.

--- backend/TuringClothes/TuringClothes/Repository/TemporaryOrderRepository.cs
+++ backend/TuringClothes/TuringClothes/Repository/TemporaryOrderRepository.cs
@@ -1,46 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TuringClothes.Database;
-using TuringClothes.Model;
-
-namespace TuringClothes.Repository
+﻿namespace TuringClothes.Repository
 {
     public class TemporaryOrderRepository
     {
-        private readonly MyDatabase _myDatabase;
-        private readonly ProductRepository _productRepository;
-
-
-        public TemporaryOrderRepository(MyDatabase database, ProductRepository productRepository)
-        {
-            _myDatabase = database;
-            _productRepository = productRepository;
-
-        }
-
-        public async Task AddTemporaryOrderAsync(long userId, ICollection<CartDetail> cartDetails)
-        {
-            var orderDetails = cartDetails.Select(cart => new OrderDetail
-            {
-                ProductID = cart.ProductId,
-                Amount = cart.Amount,
-                Product = cart.Product
-            }).ToList();
-
-            var temporaryOrder = new TemporaryOrder
-            {
-                UserId = userId,
-                Details = orderDetails
-            };
-
-            /*
-             * DESCONTAR EL STOCK DE LOS PRODUCTOS RESERVADOS
-             * ANTES DE GUARDAR EN LA BASE DE DATOS
-             */
-
-            await _myDatabase.TemporaryOrders.AddAsync(temporaryOrder);
-            await _myDatabase.SaveChangesAsync();
-        }
-
     }
 }
