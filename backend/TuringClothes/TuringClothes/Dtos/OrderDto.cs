90d73938bfabdd19f07a0b893614a77cade11325
Jricnav1811 <jricnav1811@g.educaand.es>
2024-11-26 19:59:25 +0100

    Me queda el getStatus

---
+++ backend/TuringClothes/TuringClothes/Dtos/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using Stripe.Checkout;
+
+namespace TuringClothes.Dtos
+{
+    public class OrderDto
+    {
+        public long OrderId { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
+        public int Total { get; set; }
+    }
+}
