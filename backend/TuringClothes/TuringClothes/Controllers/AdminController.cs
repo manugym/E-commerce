99c77c20ef8ca6dbbbf2e95635e61317101863cd
albacabsan <145496884+albacabsan@users.noreply.github.com>
2024-12-04 02:48:56 +0100

    productos y usuarios en vista admin + imageService

--- backend/TuringClothes/TuringClothes/Controllers/AdminController.cs
+++ backend/TuringClothes/TuringClothes/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TuringClothes.Database;
 
 namespace TuringClothes.Controllers
 {
@@ -7,5 +8,44 @@ namespace TuringClothes.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private MyDatabase _mydatabase;
+        public AdminController(MyDatabase myDatabase) 
+        {
+            _mydatabase = myDatabase;
+        }
+
+        [HttpGet("getAllUsers")]
+        public IActionResult GetUsers()
+        {
+            var users = _mydatabase.Users
+                .Select(user => new
+                {
+                    user.Id,
+                    user.Name,
+                    user.Surname,
+                    user.Email,
+                    user.Role
+                })
+                .ToList();
+
+            return Ok(users);
+        }
+
+        [HttpGet("getAllProducts")]
+        public IActionResult GetProducts()
+        {
+            var products = _mydatabase.Products
+                .Select(product => new
+                {
+                    product.Id,
+                    product.Name,
+                    product.Price,
+                    product.Stock,
+                    product.Image
+                })
+                .ToList();
+
+            return Ok(products);
+        }
     }
 }
