744389513dac38a74c75c9b6e30b8c1d1668b366
Manuel José Muñoz Martín <mmunmar594@cpifpalanturing.es>
2024-12-05 20:56:01 +0100

    Revert "CORS"
    This reverts commit f8ab1e7999a292b5117ff33819caac75a37451b9.

--- backend/TuringClothes/TuringClothes/Program.cs
+++ backend/TuringClothes/TuringClothes/Program.cs
@@ -102,17 +102,19 @@ namespace TuringClothes
             StripeConfiguration.ApiKey = "sk_test_51QJzkGRqNFmfQiA9GJ41Y9yoGmgUsXLzdV8mbk06Rtfj5Q7d5Z0F0foXulLkkIKeRGpyGy5L4zM6UwtbprVlNbp300qCldYM25";
 
 
+            if (builder.Environment.IsDevelopment())
+            {
                 //Permite CORS
                 builder.Services.AddCors(options =>
                 {
                     options.AddDefaultPolicy(builder =>
                     {
-                        builder.AllowAnyOrigin()
+                        builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
                         .AllowAnyHeader()
                         .AllowAnyMethod();
                     });
                 });
-            
+            }
             var app = builder.Build();
 
             app.UseStaticFiles();
@@ -121,9 +123,9 @@ namespace TuringClothes
             if (app.Environment.IsDevelopment()) { 
                 app.UseSwagger();
                 app.UseSwaggerUI();
-                //permite CORS   
+                //permite CORS
+                app.UseCors();
             }
-            app.UseCors();
             SeedDatabase(app.Services);
             app.UseHttpsRedirection();
 
